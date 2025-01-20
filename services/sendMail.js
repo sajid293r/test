ad1ebaeed981c5ca845e23d22b1a0db43aa7025e
sajid293r <sajid293r@gmail.com>
2025-01-21 04:20:36 +0500

    Update sendMail.js

--- services/sendMail.js
+++ services/sendMail.js
@@ -1,24 +1,24 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST, 
+    host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_PORT == 465, // true for port 465, false for other ports
-    requireTLS: true, 
+    secure: process.env.SMTP_PORT == 465, 
+    requireTLS: true,
     auth: {
-        user: process.env.SMTP_USER, 
-        pass: process.env.SMTP_PASS  
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     },
     logger: true,
     debug: true
 });
 
-const sendEmail = (from, recipient, subject, text, delay) => {
+const sendEmail = (from, recipient, subject, html, delay) => {
     const message = {
         from,
         to: recipient,
         subject,
-        text
+        html 
     };
 
     setTimeout(async () => {

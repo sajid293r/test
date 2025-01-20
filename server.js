ab865938b686309e65f21e1e650e34fe35af7b4c
unknown <mrizwan.bytecraft@gmail.com>
2025-01-20 22:42:07 +0500

    done

--- server.js
+++ server.js
@@ -1,22 +1,83 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
-const port = 3000;
+const port = 5000;
 
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// Define a route for the root URL
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST, 
+    port: process.env.SMTP_PORT,
+    secure: false, 
+    auth: {
+        user: process.env.SMTP_USER, 
+        pass: process.env.SMTP_PASS  
+    }
 });
 
-// Define a route for a POST request
-app.post('/data', (req, res) => {
-    const data = req.body;
-    res.json({ message: 'Data received!', data });
+mongoose.connect('mongodb://localhost:27017/emailScheduler', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-// Start the server
+app.use(bodyParser.json());
+app.use(cors());
+
+const EmailSchema = new mongoose.Schema({
+    from: String,
+    to: [String], 
+    subject: String,
+    text: String,
+    sendAt: Date
+});
+
+const Email = mongoose.model('Email', EmailSchema);
+
+app.get("/", (req, res) => {
+    res.send("Server is Running");
+});
+
+app.post('/scheduleEmail', async (req, res) => {
+    console.log(req.body)
+    const { from, to, subject, text, sendAt, gap } = req.body;
+    const uniqueId = uuidv4();
+    const email = new Email({ from, to, subject, text, sendAt });
+
+    await email.save();
+
+    const sendEmail = async (recipient, delay) => {
+        const message = {
+            from,
+            to: recipient,
+            subject,
+            text
+        };
+
+        setTimeout(async () => {
+            try {
+                await transporter.sendMail(message);
+                console.log('Email sent to', recipient);
+            } catch (error) {
+                console.error('Error sending email to', recipient, ':', error.message);
+            }
+        }, delay);
+    };
+
+    const recipients = Array.isArray(to) ? to.map(email => email.trim()) : [to.trim()];
+    let delay = 0;
+
+    for (const recipient of recipients) {
+        sendEmail(recipient, delay);
+        delay += gap * 60000;
+    }
+
+    res.send('Email scheduled successfully');
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port http://localhost:${port}`);
 });

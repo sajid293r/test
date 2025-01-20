3161ba0f537d16eb633003add77f3e28662587f2
unknown <mrizwan.bytecraft@gmail.com>
2025-01-21 01:13:28 +0500

    done

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const port = 5000;
 mongoose.connect('mongodb+srv://user123:user123@cluster0.unkh6.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-   
+    connectTimeoutMS: 30000, // Increase connection timeout
+    socketTimeoutMS: 30000 // Increase socket timeout
 });
-
+app.get("/", (req,res)=>{
+    res.send("Hello World")
+})
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/', emailRoutes);
+app.use('/api', emailRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);

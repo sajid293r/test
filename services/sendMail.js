044e7158f3f9319dccaadcc588adfbfd4a79cc50
unknown <mrizwan.bytecraft@gmail.com>
2025-01-23 01:27:20 +0500

    done

--- services/sendMail.js
+++ services/sendMail.js
@@ -1,12 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    secure:465, 
+    host: smtp.gmail.com,
+    secure: 465, 
     requireTLS: true,
     auth: {
-        user: "danialhamza.bytecraft@gmail.com",
-        pass:"ypdi wdqk ekln fldw"
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     },
 
 });

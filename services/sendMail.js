9ebfb0615bf5f645e4a827e3f9f24d627d3c78c3
unknown <mrizwan.bytecraft@gmail.com>
2025-01-23 01:15:22 +0500

    done

--- services/sendMail.js
+++ services/sendMail.js
@@ -9,6 +9,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
     },
+
 });
 
 const sendEmail = (from, recipient, subject, html, delay) => {
